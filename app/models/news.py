52eb64b7bb91668fec5b41c2f6f817dab38b0525
ZippMukami <zimukami@gmail.com>
2022-05-03 19:33:16 +0300

    defines a news class.

--- app/models/news.py
+++ app/models/news.py
@@ -1,1 +1,13 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self, id, title, description, urlToImage, content, publishedAt):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.content = content
+        self.publishedAt = publishedAt
 
