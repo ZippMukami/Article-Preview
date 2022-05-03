5c479bbb134014978bc8cd9977320907ae14e24c
ZippMukami <zimukami@gmail.com>
2022-05-03 20:06:50 +0300

    imports get_news function from the request module.

--- app/views.py
+++ app/views.py
@@ -7,9 +7,16 @@ from .request import get_news
 #views
 @app.route('/')
 def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting top headlines
+    top_headlines = get_news('headlines')
+    print(top_headlines)
 
     title = 'Home - Welcome to Article Review. This should be it!'
-    return render_template ('index.html', title = title)
+    return render_template ('index.html', title = title, headlines = top_headlines)
 
 
 @app.route('/news/<int:news_id>')
