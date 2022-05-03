b187c88d93f0a3c39315c46ad665ee15b4e63a02
ZippMukami <zimukami@gmail.com>
2022-05-03 18:37:03 +0300

    adds class config

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ def index():
      return render_template ('index.html', message = message)
 
 
-@app.route('/news/<new_id>')
+@app.route('/news/<int:news_id>')
 def news(news_id):
 
      return render_template('news.html', id = news_id)
