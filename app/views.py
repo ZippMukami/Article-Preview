62a4233a7bce7bd97d73407b3c8a9b4797eee0fb
ZippMukami <zimukami@gmail.com>
2022-05-05 02:52:16 +0300

    updates index view function with request object, redirect and url_for functions.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from curses import tigetflag
 from email import message
 from turtle import title
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 from .request import get_bulletins, get_bulletin, search_bulletin
 
@@ -18,7 +18,14 @@ def index():
     everything = get_bulletins('everything')
     
     title = 'Home - welcome to Article Review!'
-    return render_template('index.html', title = title, headlines = top_headlines, everything = everything)
+
+    search_bulletin = request.args.get('bulletin_query')
+
+    if search_bulletin:
+        return redirect(url_for('search', bulletin_name = search_bulletin))
+    else:
+
+        return render_template('index.html', title = title, headlines = top_headlines, everything = everything)
 
 @app.route('/bulletin/<int:id>')    
 def bulletin(id):
