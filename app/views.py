9bbcb4faf3d6f9d6a394c377985b85be9a47d503
ZippMukami <zimukami@gmail.com>
2022-05-05 01:20:48 +0300

    updates bulletin details route.

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from turtle import title
 from flask import render_template
 from app import app
 from .request import get_bulletins
+from .requests import get_bullets, get_bulletin
 
 
 # Views
@@ -20,10 +21,13 @@ def index():
     title = 'Home - welcome to Article Review!'
     return render_template('index.html', title = title, headlines = top_headlines, everything = everything)
 
-@app.route('/bulletin/<int:bulletin_id>')    
-def bulletin(bulletin_id):
+@app.route('/bulletin/<int:id>')    
+def bulletin(id):
     '''
     View bulletin page function that returns the bulletin page and its dat
     '''
 
-    return render_template('bulletin.html', id = bulletin_id)
+    bulletin = get_bulletin(id)
+    title = f'{bulletin.title}'
+
+    return render_template('bulletin.html', title = title, bulletin = bulletin)
