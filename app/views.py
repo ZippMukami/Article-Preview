db73bbdd5cc7581f7b20318532d8f3d753c22c87
ZippMukami <zimukami@gmail.com>
2022-05-04 18:39:02 +0300

    adds and connects configuration file.

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
+from curses import tigetflag
 from email import message
+from turtle import title
 from flask import render_template
 from app import app
 
@@ -9,12 +11,13 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message = "Articles Review Page"
-    return render_template('index.html', message = message)
+    title = 'Home - welcome to Article Review!'
+    return render_template('index.html', title = title)
 
 @app.route('/bulletin/<int:bulletin_id>')    
 def bulletin(bulletin_id):
     '''
     View bulletin page function that returns the bulletin page and its dat
     '''
+
     return render_template('bulletin.html', id = bulletin_id)
