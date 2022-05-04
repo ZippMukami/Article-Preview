0ee93da492af0def6951417e928a690158b4d89d
ZippMukami <zimukami@gmail.com>
2022-05-04 18:12:11 +0300

    adds a bulletin page function.

--- app/views.py
+++ app/views.py
@@ -11,3 +11,10 @@ def index():
 
     message = "Articles Review Page"
     return render_template('index.html', message = message)
+
+@app.route('/bulletin/<bulletin_id>')    
+def bulletin(bulletin_id):
+    '''
+    View bulletin page function that returns the bulletin page and its dat
+    '''
+    return render_template('bulletin.html', id = bulletin_id)
