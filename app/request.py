4688da7dcf1320e25340abe491ff6d07f0302ce7
ZippMukami <zimukami@gmail.com>
2022-05-04 23:37:36 +0300

    adds base url and imports modules.

--- app/request.py
+++ app/request.py
@@ -1,4 +1,9 @@
 from app import app
+import urllib.request, json
+from .models import bulletin
 
 #Getting api key
 api_key = app.config['BULLETIN_API_KEY']
+
+#Getting the bulletin base url
+base_url = app.config["BULLETIN_API_BASE_URL"]
