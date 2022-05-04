353aedbfa35e0c880e105171065f8dfd64c0dd9d
ZippMukami <zimukami@gmail.com>
2022-05-04 23:26:12 +0300

    adds arguments in bulletin class.

--- app/bulletin_test.py
+++ app/bulletin_test.py
@@ -11,7 +11,7 @@ class BulletinTest(unittest.TestCase):
         '''
         Set up method that will run before every test
         '''
-        self.new_bulletin = Bulletin(1234)
+        self.new_bulletin = Bulletin(1234,"Federal Reserve issues FOMC statement - Federal Reserve", "Although overall economic activity edged down in the first quarter, household spending and business fixed investment remained strong. Job gains have been robus","","Although overall economic activity edged down in the first quarter, household spending and business fixed investment remained strong. Job gains have been robust in recent months, and the unemployment… [+2268 chars]","2022-05-04T18:08:44Z")
 
         def test_instance(self):
             self.asserTrue(isinstance(self.new_bulletin, Bulletin))
