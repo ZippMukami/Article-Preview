e70215b52d1b37022695c15314f573e88b97e81c
ZippMukami <zimukami@gmail.com>
2022-05-07 01:20:25 +0300

    registering a Blueprint inside our application factory function.

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,18 +1,29 @@
-from ensurepip import bootstrap
-from flask import Flask
-from .config import DevConfig
-from flask_bootstrap import Bootstrap
-
-#Initializing application
-app = Flask(__name__, instance_relative_config = True)
+from flask import Blueprint, Flask
+from pip import main
+main = Blueprint('main',__name__)
+from . import view, errors
 
 
-# setting up configuration 
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
 
-# Initializing flask extensions
-bootstrap = Bootstrap(app)
 
-from app import views
-from app import error
+
+
+
+# from ensurepip import bootstrap
+# from flask import Flask
+# from .config import DevConfig
+# from flask_bootstrap import Bootstrap
+
+# #Initializing application
+# app = Flask(__name__, instance_relative_config = True)
+
+
+# # setting up configuration 
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
+
+# # Initializing flask extensions
+# bootstrap = Bootstrap(app)
+
+# from app import views
+# from app import error
